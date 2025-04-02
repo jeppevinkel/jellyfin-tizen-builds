368f4fdaa63d111a148e8c62b1960922ba901572
Jeppe Beier <jeppevinkel@gmail.com>
2025-04-02 22:30:46 +0200

    Experimental matrix generation

--- scripts/Check-Updates.ps1
+++ scripts/Check-Updates.ps1
@@ -17,8 +17,31 @@ $versions.commits | ForEach-Object {
     Write-Host "Checking: $($commit.owner)/$($commit.repo)@$($commit.ref)"
     Write-Host "Updates:  $($commit.latest -eq $response.sha)\n"
 
+    $commit.latest = $response.sha
+
     if ($newCommits) {
-        $updates += "New commit to $($commit.ref) https://github.com/$($commit.owner)/$($commit.repo)/commit/$($response.sha)"
+        $updates += "New commit to $($commit.ref) https://github.com/$($commit.owner)/$($commit.repo)/commit/$($commit.latest)"
+    }
+}
+
+$versions.releases | ForEach-Object {
+    $release = $_
+
+    $headers = @{
+        "Accept"               = "application/vnd.github+json"
+        "Authorization"        = "Bearer $Env:GITHUB_TOKEN"
+        "X-GitHub-Api-Version" = "2022-11-28"
+    }
+    $response = Invoke-RestMethod -Uri "https://api.github.com/repos/$($release.owner)/$($release.repo)/releases/latest" -Headers $headers
+    $newRelease = $release.latest -ne $response.tag_name
+
+    Write-Host "Checking: $($release.owner)/$($release.repo)@latest"
+    Write-Host "Updates:  $($release.latest -eq $response.tag_name)\n"
+
+    $release.latest = $response.tag_name
+
+    if ($newRelease) {
+        $updates += "New $($release.repo) release\n"
     }
 }
 
@@ -31,6 +54,29 @@ else {
     Add-Content -Path $Env:GITHUB_OUTPUT -Value "triggerBuild=false"
 }
 
-# $versions.PSObject.Properties | ForEach-Object {
-#     Add-Content -Path $Env:GITHUB_OUTPUT -Value "$($_.Name)=$($_.Value)"
-# }
+$matrixDefinition = Get-Content -Path ./matrix.json | ConvertFrom-Json
+$matrix = @{
+    include = @()
+}
+
+$matrixDefinition.variations | ForEach-Object {
+    $variation = $_
+
+    $versions.commits | ForEach-Object {
+        $commit = $_
+
+        $matrixTask = @{
+            tag = $commit.ref
+            repository = "jellyfin/jellyfin-web"
+            artifact_name = "Jellyfin-$($commit.ref)-$($variation.name)"
+        }
+
+        $variation.extra_values | ForEach-Object {
+            $matrixTask | Add-Member -NotePropertyName $_.key -NotePropertyValue $_.value
+        }
+
+        $matrix.include += $matrixTask
+    }
+}
+
+Write-Host $matrix | ConvertTo-Json -Depth 10
