bbc4cb2742f784228f4fbcce62eaeda8e3a74cee
jeppevinkel <jeppevinkel@gmail.com>
2025-04-03 23:09:07 +0200

    Update Check-Updates.ps1

--- scripts/Check-Updates.ps1
+++ scripts/Check-Updates.ps1
@@ -20,7 +20,7 @@ $versions.commits | ForEach-Object {
     $commit.latest = $response.sha
 
     if ($newCommits) {
-        $updates += "New commit to $($commit.ref) https://github.com/$($commit.owner)/$($commit.repo)/commit/$($commit.latest)\n\>$($response.commit.message | ConvertTo-Json)"
+        $updates += "New commit to $($commit.ref) https://github.com/$($commit.owner)/$($commit.repo)/commit/$($commit.latest)\n\>$($response.commit.message.replace("`n", "\n"))"
     }
 }
 
